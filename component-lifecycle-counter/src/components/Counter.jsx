b02238a6d710c80a904883edb7d6e1373becdb31
BagusAdiSaputra <bagusadisaputra04@gmail.com>
2024-06-05 20:42:03 +0700

    upload proyek lifecycle counter

---
+++ component-lifecycle-counter/src/components/Counter.jsx
@@ -0,0 +1,62 @@
+// Import useEffect
+import React, { useState, useEffect } from 'react';
+
+const Counter = ({ finishGame }) => {
+  const [count, setCount] = useState(0);
+
+  // Tambah keydown event listener
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyPress);
+  }, [count]);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  // Tambahkan keydown event handler
+  const handleKeyPress = (event) => {
+    console.log('key pressed'); // Tambahkan console.log disini
+    const { key } = event;
+    if (key === 'Enter') {
+      setCount(count + 1);
+    }
+  };
+
+  return (
+    <div>
+      <h1 style={{ fontSize: '64px' }}>{count}</h1>
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          handleClick();
+        }}
+      >
+        +
+      </button>
+      <div>
+        <button
+          onClick={() => {
+            finishGame();
+          }}
+          style={buttonStyle}
+        >
+          Finish Game
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const buttonStyle = {
+  fontSize: '32px',
+  width: '200px',
+  height: '48px',
+  borderRadius: '4px',
+  marginRight: '24px',
+  marginLeft: '24px',
+  marginTop: '30px',
+  color: '#fff',
+  backgroundColor: '#66ccff',
+};
+
+export default Counter;
